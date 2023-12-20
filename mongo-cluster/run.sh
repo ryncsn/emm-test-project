f6b7396e43f348eacc3cea17143ca92ab6441028
Kairui Song <ryncsn@gmail.com>
2023-12-20 17:01:20 +0800

    Update mongo runner
    Use this to restart existing cluster
    
    Signed-off-by: Kairui Song <ryncsn@gmail.com>

--- mongo-cluster/run.sh
+++ mongo-cluster/run.sh
@@ -2,6 +2,7 @@
 
 port=7000
 for r in r1 r2 r3; do
-	sudo docker run -d --replace --network mongo-cluster --name "mongo-$r" -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=passwd -e MONGO_INITDB_DATABASE=testdb -v /var/data/mongo-$r:/data/db -p $port:27017 mongo -f /data/db/config.conf
+	port=$(( port + 1 ))
+	sudo docker run -d --replace --network mongo-cluster --name "mongo-$r" -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=passwd -e MONGO_INITDB_DATABASE=testdb -v /var/mongo-test/mongo-$r:/data/db -p $port:27017 mongo:6 -f /data/db/config.conf
 	sleep 2
 done
